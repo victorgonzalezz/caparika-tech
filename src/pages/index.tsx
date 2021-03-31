2a9aaf9f47b13e7345d066e9a197d328e8f640e1
Victor <vitopcg@gmail.com>
2021-03-31 22:41:58 +0100

    commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+  
+import { TextStyle } from '@shopify/polaris';
+
+const Index = () => (
+  <div>
+    <p>Sample app using React and Next.js</p>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
+  </div>
+);
+
+export default Index;
